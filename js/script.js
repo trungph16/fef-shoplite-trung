b15382a526820c59d341c8e2ba4d0344320364f3
Trunh <trung1620460@gmail.com>
2026-06-12 14:17:46 +0700

    init project and basic folder structure

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Shopping cart functions
+function addToCart() {
+    alert('Product added to cart!');
+    window.location.href = 'cart.html';
+}
+
+function checkout() {
+    alert('Proceeding to checkout...');
+}
+
+function registerUser() {
+    alert('User registered successfully!');
+}
